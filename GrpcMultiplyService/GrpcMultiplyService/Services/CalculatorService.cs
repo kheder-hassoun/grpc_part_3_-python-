456b390f9f7bae411f73d2b584e16daac9834866
kheder Hassoun <103248209+kheder-hassoun@users.noreply.github.com>
2024-10-30 03:11:18 +0300

    first and final

---
+++ GrpcMultiplyService/GrpcMultiplyService/Services/CalculatorService.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcMultiplyService.Services;
+
+namespace GrpcMultiplyService.Services
+{
+    public class CalculatorServiceImpl: GrpcMultiplyService.Services.CalculatorService.CalculatorServiceBase
+    {
+        public override Task<CalculationResult> Multiply(TwoNumbers request, ServerCallContext context)
+        {
+            int result = request.Num1 * request.Num2;
+
+            return Task.FromResult(new CalculationResult { Result = result });
+        }
+    }
+}
